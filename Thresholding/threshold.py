854a23e031d13638128ccba0745d70f38d36cde2
Khantanjil <tanjilkhan.sh@gmail.com>
2020-03-18 15:14:10 +0000

    Dataframe added, better detection

--- Thresholding/threshold.py
+++ Thresholding/threshold.py
@@ -13,17 +13,36 @@ while True:
         continue
 
     delta_frame = cv2.absdiff(first_frame, gray)
+
     thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
     thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
 
     thresh_frame_a = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY_INV)[1]
     thresh_frame_a = cv2.dilate(thresh_frame_a, None, iterations=2)
 
+    thresh_frame_b = cv2.threshold(delta_frame, 127, 255, cv2.THRESH_BINARY)[1]
+    thresh_frame_b = cv2.dilate(thresh_frame_b, None, iterations=2)
+
+    thresh_frame_c = cv2.threshold(delta_frame, 127, 255, cv2.THRESH_BINARY_INV)[1]
+    thresh_frame_c = cv2.dilate(thresh_frame_c, None, iterations=2)
+
+    thresh_frame_d = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_TOZERO)[1]
+    thresh_frame_d = cv2.dilate(thresh_frame_d, None, iterations=2)
+
+    thresh_frame_e = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_TRUNC)[1]
+    thresh_frame_e = cv2.dilate(thresh_frame_e, None, iterations=2)
+
     # Show images
     cv2.imshow("DELTA", delta_frame)
+    # No
     cv2.imshow("Thresh BINARY/30", thresh_frame)
-    cv2.imshow("THRESH BINARY_INV", thresh_frame_a)
-
+    # Yes (room)
+    cv2.imshow("THRESH BINARY_INV/30", thresh_frame_a)
+    # test
+    cv2.imshow("THRESH BINARY/127", thresh_frame_b)
+    cv2.imshow("THRESH BINARY_INV/127", thresh_frame_c)
+    cv2.imshow("THRESH TOZERO/30", thresh_frame_d)
+    cv2.imshow("TRESH TRUNC/30", thresh_frame_e)
     key = cv2.waitKey(1)
     if key == ord('q'):
         break
--- moviment.py
+++ moviment.py
@@ -1,14 +1,21 @@
 # Import open-cv2 python module
 import cv2
+import time
+from datetime import datetime
+import pandas
 
 # Webcam
 video = cv2.VideoCapture(0)
 first_frame = None
+status_list = [None, None]
+times = []
+df = pandas.DataFrame(columns=["Start", "End"])
 
 # Infinite looop
 while True:
     # Loads the webcam camera
     check, frame = video.read()
+    status = 0
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
@@ -17,16 +24,27 @@ while True:
         continue
 
     delta_frame = cv2.absdiff(first_frame, gray)
-    thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY_INV)[1]
+    # On my bedroom
+    thresh_frame = cv2.threshold(delta_frame, 200, 255, cv2.THRESH_BINARY)[1]
+
+    # on my room
+    # thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY_INV)[1]
     thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
 
     (_, cnts, _) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in cnts:
-        if cv2.contourArea(contour) < 1000:
+        if cv2.contourArea(contour) < 10000:
             continue
+        status = 1
+
         (x, y, w, h) = cv2.boundingRect(contour)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+    status_list.append(status)
+    if status_list[-1] == 1 and status_list[-2] == 0:
+        times.append(datetime.now())
+    if status_list[-1] == 0 and status_list[-2] == 1:
+        times.append(datetime.now())
 
     # Delta
     cv2.imshow("DELTA FRAME", delta_frame)
@@ -37,11 +55,19 @@ while True:
     # Detect objects
     cv2.imshow("Image", frame)
 
-    print(gray)
-    print(delta_frame)
+    # print(gray)
+    # print(delta_frame)
     key = cv2.waitKey(1)
     if key == ord('q'):
+        if status == 1:
+            times.append(datetime.now())
         break
 
+print(status_list)
+print(times)
+for i in range(0,len(times), 2):
+    df = df.append({"Start":times[i],"End":times[i+1]},ignore_index=True)
+
+df.to_csv("Times.csv")
 video.release()
 cv2.destroyAllWindows()
